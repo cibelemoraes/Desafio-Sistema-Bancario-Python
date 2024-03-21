986eed14d0dd233beb368a4cf8e6ea26833368a5
Your Name <you@example.com>
2024-03-20 22:14:33 -0300

    parte 3

--- desafio.py
+++ desafio.py
@@ -1,3 +1,6 @@
+from tkinter import Menu
+
+
 menu = """
 
 [d] Depositar
@@ -25,9 +28,41 @@ while True:
         else:
           print("Operação falho! O valor informado é invalido.")
 
-    elif opcao == "s":
-        valor = float(input("Informe o valor do saque"))
-    
-    execedeu_saldo = valor > saldo
-    execedeu_saldo = valor > saldo
-    execedeu_saldo = valor > saldo
+     elif opcao == "s":
+         
+         valor = float(input("informe o valor do saque: "))
+     
+         excedeu_saldo = valor > saldo
+
+         excedeu_limite = valor > limite
+ 
+         excedeu_saques = numeros_saques
+
+     if excedeu_saldo:
+         print("Operação falhou! você não possui saldo suficiente.")
+
+     elif excedeu_limite:
+         print("Operação falhou! O valor do saque excedeu o limite.")
+
+     elif excedeu_saques:
+         print("Operação falhou! numero maximo de saques excedido. ")
+
+     else:
+         print("Operação falhou! O valor informado é inválido. ")
+  
+    elif opcao == "e": 
+        print("\n ========== Extrato =======")     
+        print("Não foram realizadas movimentações." if not extrato else extrato)
+        print(f"\n Saldo: R$"{saldo:.2f})
+        print("===============================")
+
+    elif opcao == "q"
+        break
+
+    else:
+        print("Operação invalida, por favor selecione novamente a operação desejada.")      
+
+
+
+
+
